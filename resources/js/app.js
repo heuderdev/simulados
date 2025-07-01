60895954d2bf1566844c93c8767f4997e1c1aaf5
heuderdev <heuderdev@gmail.com>
2025-07-01 20:54:27 -0300

    feat: instalação de autenticação com o liveware

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
+import { Livewire, Alpine } from "../../vendor/livewire/livewire/dist/livewire.esm"
+import Clipboard from '@ryangjchandler/alpine-clipboard'
+
+Alpine.plugin(Clipboard) 
+Livewire.start()
+
+
 import './bootstrap';
+
+
